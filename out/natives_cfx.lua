703d89c79e06a8829baaa6b8321792a6c647a610
Gottfried Leibniz <37632412+gottfriedleibniz@users.noreply.github.com>
2020-05-05 13:43:37 -0300

    experimental: include output

---
+++ out/natives_cfx.lua
@@ -0,0 +1,12 @@
+-- Native definitions
+
+native "SET_TEXT_FONT_FOR_CURRENT_COMMAND"
+	arguments {
+		int "fontId",
+	}
+	ns "CFX"
+    apiset "client"
+	returns "void"
+	doc [[!
+<param name="fontId">:</param>
+	]]
